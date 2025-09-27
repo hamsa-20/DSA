67f1ed090ce48f0a8078aa2ac1ba74f2f034f65f
Hamsa Dixit <147731006+hamsa-20@users.noreply.github.com>
2025-09-27 11:29:31 +0530

    Time: 2 ms (88.11%), Space: 62.5 MB (82.93%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.java
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -1,33 +1,15 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int l = nums1.length <= nums2.length ? nums1.length : nums2.length;
-        int len = nums1.length >= nums2.length ? nums1.length : nums2.length;
-
-        for (int i = 0; i < l; i++) {
-            int start = 0;
-            int end = len - 1;
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-
-                if (nums1.length > nums2.length) {
-                    if (nums2[i] == nums1[mid]) {
-                        return nums2[i];
-                    } else if (nums1[mid] > nums2[i]) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
-                } else {
-                    if (nums1[i] == nums2[mid]) {
-                        return nums1[i];
-                    } else if (nums2[mid] > nums1[i]) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
-                }
+        int i=0;
+        int j=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }else if(nums1[i]>nums2[j]){
+                    j++;
+            }else{
+                i++;
             }
         }
-        return -1;
-    }
+   return -1; }
 }
