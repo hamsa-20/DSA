43911128f50bc0f48a14489ceef0f863da7c1b3c
Hamsa Dixit <147731006+hamsa-20@users.noreply.github.com>
2025-11-22 20:16:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,12 @@
-class Solution {
-    public int maxProfit(int[] prices) {
+
+class Solution{
+    public int maxProfit(int[] prices){
         int min=prices[0];
         int max=0;
         for(int i=0;i<prices.length;i++){
             int profit=prices[i]-min;
-            max=Math.max(max,profit);
+            max=Math.max(profit,max);
             min=Math.min(min,prices[i]);
-        }return max;
-    }
+        }
+    }return max;
 }
